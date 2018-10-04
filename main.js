bf7a6b87dbc816dbe49cd2e57d22727685ffe8b1
goffh91 <35957540+goffh91@users.noreply.github.com>
2018-10-05 00:43:22 +0900

    3332/21141

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
+let qs = require('querystring');
 
-function getTemplateHTML(title, list, body){
+function getTemplateHTML(title, list, body, control){
     return (`
         <!doctype html>
         <html lang='kr'>
@@ -13,7 +14,7 @@ function getTemplateHTML(title, list, body){
         <body>
           <h1><a href="/">Home</a></h1>
           ${list}
-          <a href="/create">create</a>
+          ${control}
           ${body}
         </body>
         </html>
@@ -46,7 +47,7 @@ let app = http.createServer(
                     title = 'Hello';
                     let description = 'This is Node.js App';
                     let list = getTemplateList(fileList);
-                    let template = getTemplateHTML(title, list, `<h2>${title}</h2>${description}`);
+                    let template = getTemplateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
                     Response.writeHead(200);
                     Response.end(template);
                 });
@@ -57,7 +58,13 @@ let app = http.createServer(
                     let description = fs.readFile(`data/${queryData.id}`, 'utf8', 
                     (error, description)=>{
                         let list = getTemplateList(fileList);
-                        let template = getTemplateHTML(title, list, `<h2>${title}</h2>${description}`);
+                        let template = getTemplateHTML(title, list, `<h2>${title}</h2>${description}`, 
+                            `<a href="/create">create</a> 
+                            <a href="/update?id=${title}">update</a> 
+                            <form method="POST" action="/deleteProccess" onsubmit="alert('delete!')">
+                                <input type="hidden" name="id" value="${title}">
+                                <input type="submit" value="delete">
+                            </form>`);
                         Response.writeHead(200);
                         Response.end(template);
                     });
@@ -74,10 +81,77 @@ let app = http.createServer(
                         <p><input type="text" name="title" placeholder="title"></p>
                         <p><textarea name="description" placeholder="description"></textarea></p>
                         <p><input type="submit" value="submit">
-                        <input type="reset" value="reset"></p>
-                    </form>`);
+                        <button type="button" onclick="history.go(-1)">Cancel</button></p>
+                    </form>`,'');
                 Response.writeHead(200);
                 Response.end(template);
+            });
+        }
+        else if(pathName === '/processCreate')
+        {
+            let body = '';
+            Request.on('data', (data)=>{
+                body += data;
+                // Too much POST data, kill the connection!
+                // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                if (body.length > 1e6)
+                    request.connection.destroy();
+            });
+            Request.on('end', (data)=>{
+                let post = qs.parse(body);
+                let title = post.title;
+                let description = post.description;
+                fs.writeFile(`data/${title}`, description, 'utf8',
+                    (error)=>{
+                        Response.writeHead(302, {Location: `/?id=${title}`});
+                        Response.end();
+                    }
+                );
+            });
+        }
+        else if(pathName === '/update')
+        {
+            fs.readdir('./data', 'utf8', (error, fileList)=>{
+                let description = fs.readFile(`data/${queryData.id}`, 'utf8', 
+                (error, description)=>{
+                    title = queryData.id;
+                    let list = getTemplateList(fileList);
+                    let template = getTemplateHTML(title, list, 
+                        `<form method="POST" action="/processUpdate">
+                            <input type="hidden" name="id" value="${title}">
+                            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                            <p><textarea name="description" placeholder="description">${description}</textarea></p>
+                            <p><input type="submit" value="submit">
+                            <button type="button" onclick="history.go(-1)">Cancel</button></p>
+                        </form>`,'');
+                Response.writeHead(200);
+                Response.end(template);
+                });
+            });
+        }
+        else if(pathName === '/processUpdate')
+        {
+            let body = '';
+            Request.on('data', (data)=>{
+                body += data;
+                // Too much POST data, kill the connection!
+                // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                if (body.length > 1e6)
+                    request.connection.destroy();
+            });
+            Request.on('end', (data)=>{
+                let post = qs.parse(body);
+                let id = post.id;
+                let title = post.title;
+                let description = post.description;
+                fs.rename(`data/${id}`, `data/${title}`, (error)=>{
+                    fs.writeFile(`data/${title}`, description, 'utf8',
+                        (error)=>{
+                            Response.writeHead(302, {Location: `/?id=${title}`});
+                            Response.end();
+                        }
+                    );
+                });
             });
         }
         else

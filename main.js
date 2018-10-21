a7d44b736f771bc5d10cae8fa34adc0ac2f6f3a7
PKW <>
2018-10-22 01:13:45 +0900

    .

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let url = require('url');
 let qs = require('querystring');
 let path = require('path');
 let sanitizeHTML = require('sanitize-html');
-
+let Nightmare = require('nightmare');
+let nightmare = Nightmare({ show: true }); //electron으로 화면을 띄운다
 let template = require('./lib/template.js');
 
 let app = http.createServer(
@@ -189,7 +190,41 @@ let app = http.createServer(
                 //fs.writeFile(`data/${title}`, description, 'utf8',
                 //    (error)=>{
                         Response.writeHead(200, {'Content-Type': 'text/html'});
-                        Response.end('hi');
+
+                        nightmare
+                        .goto('http://www.daum.net')    //daum 포털 접속
+                        .type('#q', 'blueng.tistory.com')   //blueng.tistory.com 검색어 입력
+                        .click('.btn_search')   //검색 버튼을 클릭
+                        .wait('#siteColl')  //#siteColl 레이어를 기다린다
+                        .end()  //프로세스를 종료한다.
+                        .evaluate(() => {
+                            return document.querySelector('.wrap_tit a').innerHTML; //첫번째 검색 결과의 제목을 가져온다
+                        })
+                        .then(res => {
+                            console.log(res);   //검색 결과 제목을 콘솔에 출력한다
+                        })
+                        .catch(err => {
+                            console.log('err : ', err);
+                        })
+
+                        /*nightmare
+                        .goto('http://www.naver.com/')
+                        .type('#query', 'phpschool')
+                        .click('#search_btn')
+                        .wait('#main_pack')
+                        // 사이트 검색 결과의 첫 번째 링크 주소를 가져온다.
+                        .evaluate(() => document.querySelector('#main_pack div.nsite > ul > li a').href)
+                        .then(result => nightmare
+                        .goto(result) // 링크 주소로 이동한다.
+                        .click('#contentWrap div.main_talkbox a') // 게시판 링크를 클릭한다.
+                        .wait('form[name="fboardlist"]') // 게시판 목록을 기다린다.
+                        // 목록에서 첫 번째 글 제목을 가져온다.
+                        .evaluate(() => document.querySelector('form[name="fboardlist"] > table.board_table > tbody > tr[class=""] > td.subject a').innerHTML)
+                        .end())
+                        .then(result => console.log(result)) // 결과를 콘솔에 출력한다.
+                        .catch(error => console.error(error));*/
+
+                        Response.end('hello');
                 //    }
                 //);
             });
